e9862628c7294706c06ab2b1eeaf610bb279931f
Pixideus <pixideus@hotmail.com>
2026-07-30 07:54:37 +0200

    Update builds.js

--- builds.js
+++ builds.js
@@ -1,142 +1,103 @@
 const builds = [
 
     {
+        section: "Standalone Builds",
         title: "Hanging Gardens of Babylon",
-
-        category: [
-            "theme",
-            "fantasy"
-        ],
-
-        cover:
-        "https://placehold.co/900x600/171b28/FFFFFF?text=Babylon",
-
+        description: "An epic fantasy recreation inspired by the legendary Hanging Gardens of Babylon, bringing one of the Seven Wonders to life.",
+        room: "Daylight Circle Room",
+        cover: "images/babylon/cover-babylon.jpg",
         images: [
-
-            "https://placehold.co/1200x800/171b28/FFFFFF?text=Babylon+1",
-
-            "https://placehold.co/1200x800/171b28/FFFFFF?text=Babylon+2",
-
-            "https://placehold.co/1200x800/171b28/FFFFFF?text=Babylon+3"
-
+            "images/babylon/babylon1.jpg",
+            "images/babylon/babylon2.jpg",
+            "images/babylon/babylon3.jpg",
+            "images/babylon/babylon4.jpg",
+            "images/babylon/babylon5.jpg"
         ],
-
-        description:
-        "An epic fantasy recreation inspired by the legendary Hanging Gardens of Babylon.",
-
-        video:
-        "https://www.youtube.com/embed/gx798FRvx14"
-
+        youtube: "https://www.youtube.com/watch?v=gx798FRvx14"
     },
 
 
-
-
-
     {
-
-        title: "Hogwarts Common Room",
-
-        category: [
-            "interior",
-            "fantasy"
-        ],
-
-
-        cover:
-        "https://placehold.co/900x600/171b28/FFFFFF?text=Hogwarts",
-
-
+        section: "Standalone Builds",
+        title: "Getaway to Greece",
+        description: "A peaceful Greek village inspired by the whitewashed streets and blue rooftops of Santorini.",
+        room: "Evening Circle Room",
+        cover: "images/greek-village/cover-greek-village.jpg",
         images: [
-
-            "https://placehold.co/1200x800/171b28/FFFFFF?text=Hogwarts+1",
-
-            "https://placehold.co/1200x800/171b28/FFFFFF?text=Hogwarts+2"
-
+            "images/greek-village/greek-village1.jpg",
+            "images/greek-village/greek-village2.jpg",
+            "images/greek-village/greek-village3.jpg",
+            "images/greek-village/greek-village4.jpg"
         ],
-
-
-        description:
-        "A magical interior inspired by the wizarding world.",
-
-
-        video:
-        ""
-
+        youtube: "https://www.youtube.com/watch?v=sxcbPXjXkvU"
     },
 
 
-
-
-
-
     {
-
-        title: "Mini Santorini",
-
-        category: [
-            "legacy",
-            "exterior"
-        ],
-
-
-        cover:
-        "https://placehold.co/900x600/171b28/FFFFFF?text=Santorini",
-
-
+        section: "Standalone Builds",
+        title: "Inside the Tree, an Enchanted Nook",
+        description: "A cozy home hidden inside a giant enchanted tree. Climb all the way to the top to discover every corner.",
+        room: "Daylight Circle Room",
+        cover: "images/inside-the-tree/cover-tree.jpg",
         images: [
-
-            "https://placehold.co/1200x800/171b28/FFFFFF?text=Santorini+1",
-
-            "https://placehold.co/1200x800/171b28/FFFFFF?text=Santorini+2"
-
+            "images/inside-the-tree/tree1.jpg",
+            "images/inside-the-tree/tree2.jpg",
+            "images/inside-the-tree/tree3.jpg",
+            "images/inside-the-tree/tree4.jpg",
+            "images/inside-the-tree/tree5.jpg",
+            "images/inside-the-tree/tree6.jpg",
+            "images/inside-the-tree/tree7.jpg"
         ],
-
-
-        description:
-        "A small Mediterranean village inspired by the Greek islands.",
-
-
-        video:
-        ""
-
+        youtube: "https://www.youtube.com/watch?v=TvvXBZrNwtU"
     },
 
 
+    {
+        section: "Collections",
+        title: "Hogwarts - School of Magic",
+        description: "One of my first builds, this School of Magic is greatly inspired by Harry Potter's universe.",
+        room: "Multiple Rooms",
+        cover: "images/hogwarts/cover-sortinghat.jpg",
+        youtube: "https://www.youtube.com/watch?v=znn1qVpnJGg",
 
-
+        children: [
+            {
+                title: "Sorting Hat",
+                cover: "images/hogwarts/sortinghat.jpg"
+            },
+            {
+                title: "Great Hall",
+                cover: "images/hogwarts/greathall1.jpg"
+            },
+            {
+                title: "Magic Library & Pensieve",
+                cover: "images/hogwarts/library1.jpg"
+            }
+        ]
+    },
 
 
     {
+        section: "Collections",
+        title: "Burtonesque Wonderland",
+        description: "Inspired by Tim Burton's universe.",
+        cover: "images/tim-burton/cover-tim-burton.jpg",
+        youtube: "https://www.youtube.com/watch?v=-0euCNUfjCo",
 
-        title: "Nevermore Academy",
-
-        category: [
-            "gothic",
-            "theme"
-        ],
-
-
-        cover:
-        "https://placehold.co/900x600/171b28/FFFFFF?text=Nevermore",
-
-
-        images: [
-
-            "https://placehold.co/1200x800/171b28/FFFFFF?text=Nevermore+1",
-
-            "https://placehold.co/1200x800/171b28/FFFFFF?text=Nevermore+2"
-
-        ],
-
-
-        description:
-        "A dark gothic academy inspired by mysterious fantasy worlds.",
-
-
-        video:
-        ""
-
+        children: [
+            {
+                title: "Nightmare Before Christmas / Sleepy Hollow",
+                cover: "images/tim-burton/tim-burton1.jpg"
+            },
+            {
+                title: "Wednesday's Bedroom",
+                cover: "images/tim-burton/Wednesday-bedroom1.jpg"
+            },
+            {
+                title: "Beetlejuice Trippy Hallway",
+                cover: "images/tim-burton/beetlejuice-hall.jpg"
+            }
+        ]
     }
 
 ];
