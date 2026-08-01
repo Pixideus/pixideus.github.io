f3898194ff29c1873e529dd36d6980accc067a2f
Pixideus <pixideus@hotmail.com>
2026-08-01 03:41:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function createCard(build) {
 
+    const column = document.createElement("div");
+
+    column.className = "col-xl-3 col-lg-4 col-md-6";
+
+
     const card = document.createElement("div");
 
     card.className = "build-card";
 
 
-   card.innerHTML = `
+    card.innerHTML = `
 
-       <img src="${build.cover}" 
-     alt="${build.title}"
-     loading="lazy">
+        <img src="${build.cover}" 
+             alt="${build.title}"
+             loading="lazy">
 
         <div class="build-content">
 
@@ -21,25 +26,29 @@ function createCard(build) {
 
     `;
 
-card.addEventListener("click", () => {
 
-    openBuild(build);
+    card.addEventListener("click", () => {
 
-});
-    return card;
+        openBuild(build);
+
+    });
+
+
+    column.appendChild(card);
+
+    return column;
 
 }
-
-
-
 function displayBuilds() {
 
     const buildsContainer = document.getElementById("builds-container");
-    const collectionsContainer = document.getElementById("collections-container");
+    const legacyContainer = document.getElementById("legacy-container");
+
 
     builds.forEach(build => {
 
         const card = createCard(build);
+
 
         if (build.section === "My Builds") {
 
@@ -47,22 +56,13 @@ function displayBuilds() {
 
         }
 
-        else if (build.section === "Collections") {
 
-            collectionsContainer.appendChild(card);
+        if (build.section === "Old Builds") {
+
+            legacyContainer.appendChild(card);
 
         }
 
     });
-
-}
-
-
-
-displayBuilds();
-
-function openBuild(build){
-
-    alert(build.title);
 
 }
