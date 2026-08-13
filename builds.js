83dec4e2ce3358a57df0311fc07dea2958249e40
Pixideus <pixideus@hotmail.com>
2026-08-13 17:53:43 +0200

    Update builds.js

--- builds.js
+++ builds.js
@@ -104,9 +104,7 @@ const builds = [
         "soon"
     ],
 
-    youtube: "https://www.youtube.com/watch?v=znn1qVpnJGg"
-},
-        youtube: "https://www.youtube.com/watch?v=znn1qVpnJGg"
+            youtube: "https://www.youtube.com/watch?v=znn1qVpnJGg"
     },
     
 
