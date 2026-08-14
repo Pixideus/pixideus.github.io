fe872e3a222434ac622db60489bd8a189babcbd5
Pixideus <pixideus@hotmail.com>
2026-08-15 00:58:22 +0200

    Update builds.js

--- builds.js
+++ builds.js
@@ -187,7 +187,7 @@ blueprints: [
         title: "Manor on the hill",
       id: "manor",
         description: "A simple black manor with spooky vibes",
-        note : "I rearranged the Manor with some easier decor to get form neighborhood vendors at the back.  Budget is 278/350, so you can add some mor decor ! I used the new black dye too, picture on main page and first picture below is the new version.",  
+        note : "I rearranged the Manor with some easier decor to get form neighborhood vendors at the back.  Budget is 278/350, so you can add some more decor ! I used the new black dye too, picture on main page and first picture below is the new version.",  
         room: "Exterior",
           blueprint:"soon",
         cover: "images/Manor/cover-manor.jpg",
